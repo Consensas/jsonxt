b4cf2cdcb351af6c0ab261508b38e1d54dd1c0d1
David Janes <david@consensas.com>
2021-04-10 10:04:08 -0400

    add header

--- python/unpack.py
+++ python/unpack.py
@@ -1,3 +1,25 @@
+#
+#   unpack.py
+#
+#   David Janes
+#   Consensas
+#   2021-04-09
+#
+#   Copyright (2015-2021) Consensas
+# 
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#  
+
 import sys
 import os
 import json
@@ -67,15 +89,16 @@ def unpack(packed, resolver_resolver):
 
     return unpack_payload(majors[4], template)
     
-__dirname__ = os.path.dirname(os.path.abspath(__file__))
-TESTS = os.path.join(__dirname__, "..", "test", "data")
+if __name__ == "__main__":
+    __dirname__ = os.path.dirname(os.path.abspath(__file__))
+    TESTS = os.path.join(__dirname__, "..", "test", "data")
 
-with open(os.path.join(TESTS, "c4-1-1-packed.txt")) as fin:
-    uri = fin.read()
+    with open(os.path.join(TESTS, "c4-1-1-packed.txt")) as fin:
+        uri = fin.read()
 
-def resolver(resolver_name, resolver_type):
-    with open(os.path.join(TESTS, "covid-templates.json")) as fin:
-        return fin.read()
+    def resolver(resolver_name, resolver_type):
+        with open(os.path.join(TESTS, "covid-templates.json")) as fin:
+            return fin.read()
 
-result = unpack(uri, resolver)
-print(json.dumps(result, indent=2))
+    result = unpack(uri, resolver)
+    print(json.dumps(result, indent=2))
