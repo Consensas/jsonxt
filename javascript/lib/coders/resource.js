a436650b7f514514db09234807564c7bf3bb0907
Vitor Pamplona <vitor@vitorpamplona.com>
2021-06-02 19:51:15 -0400

    resource prefix encoder

---
+++ javascript/lib/coders/resource.js
@@ -0,0 +1,44 @@
+/*
+ *  lib/encoders/did.web.js
+ *
+ *  Vitor Pamplona
+ *  PathCheck Foundation
+ *  2021-03-16
+ *
+ *  Copyright (2013-2021) Consensas
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _util = require("../_util")
+const NAME = "resource:"
+
+const prefix = "resource:";
+
+/**
+ */
+exports.encode = (rule, value) => {
+    return _util.prefixRemoverEncode(rule, value, prefix);
+}
+
+/**
+ */
+exports.decode = (rule, value) => {
+    return _util.prefixRemoverDecode(rule, value, prefix);
+}
+
+exports.schema = {
+    type: "string",
+}
