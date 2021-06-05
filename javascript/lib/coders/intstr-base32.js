9456062acbd0585c525b132055ac93700b4a8978
Vitor Pamplona <vitor@vitorpamplona.com>
2021-06-05 17:41:52 -0400

    Adding a Integer As String to Base32 encoder.

---
+++ javascript/lib/coders/intstr-base32.js
@@ -0,0 +1,113 @@
+/*
+ *  lib/encoders/string-base32.js
+ *
+ *  Vitor Pamplona
+ *  PathCheck Foundation
+ *  2021-03-16
+ *
+ *  Copyright (2013-2021) Consensas
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _util = require("../_util")
+const NAME = "string-base32"
+
+/**
+ *  TESTING ONLY (for now, anyway)
+ */
+/* istanbul ignore next */
+exports.encode = (rule, value) => {
+    const jsonxt = require("..")
+
+    if (_util.isNull(value)) {
+        return rule.NULL || jsonxt.ENCODE.NULL
+    } else if (_util.isUndefined(value)) {
+        return rule.UNDEFINED || jsonxt.ENCODE.UNDEFINED
+    } else if (!_util.isString(value)) {
+        throw new Error(`${NAME}: expected value to be string for ${rule.path} (got "${value}")`)
+    }
+
+    const _encoder = s => _util.integer_to_base32(parseInt(s))
+
+    if (rule.compact && (rule.compact.indexOf(value) > -1)) {
+        return jsonxt.ENCODE.ESCAPE + _util.integer_to_base32(rule.compact.indexOf(value))
+    }
+
+    if (rule.prefix) {
+        for (let pi = 0; pi < rule.prefix.length && pi < 32; pi++) {
+            const prefix = rule.prefix[pi]
+            if (!value.startsWith(prefix)) {
+                continue
+            }
+
+            return jsonxt.ENCODE.ESCAPE + _util.integer_to_base32(pi) + _encoder(value.substring(prefix.length))
+        }
+    }
+
+    if (value === "") {
+        return rule.EMPTY_STRING || jsonxt.ENCODE.EMPTY_STRING
+    } else if (value.startsWith(jsonxt.ENCODE.ESCAPE)) {
+        return jsonxt.ENCODE.ESCAPE + jsonxt.ENCODE.ESCAPE + _encoder(value.substring(1))
+    } else {
+        return _encoder(value)
+    }
+}
+
+/**
+ */
+/* istanbul ignore next */
+exports.decode = (rule, value) => {
+    const jsonxt = require("..")
+
+    if ((value === rule.NULL) || (value === jsonxt.ENCODE.NULL)) {
+        return null
+    } else if ((value === rule.UNDEFINED) || (value === jsonxt.ENCODE.UNDEFINED)) {
+        return undefined
+    } else if ((value === rule.EMPTY_STRING) || (value === jsonxt.ENCODE.EMPTY_STRING)) {
+        return ""
+    }
+
+    const _decoder = s => _util.base32_to_integer(s).toString();
+
+    if (value.startsWith(jsonxt.ENCODE.ESCAPE)) {
+        if (value[1] === jsonxt.ENCODE.ESCAPE) {
+            value = value.substring(2)
+            value = jsonxt.ENCODE.ESCAPE + _decoder(value)
+        } else {
+            if (rule.compact) {
+                const index = _util.base32_to_integer(value.substring(1))
+                if ((index >= 0) && (index < rule.compact.length)) {
+                    return rule.compact[index]
+                }
+            } else if (rule.prefix) {
+                const index = _util.base32_to_integer(value.substring(1, 2))
+                if ((index >= 0) && (index < rule.prefix.length)) {
+                    return rule.prefix[index] + _decoder(value.substring(2))
+                }
+            }
+            
+            throw new Error(`did not understand escape sequence for "${rule.path}" "${value}"`)
+        }
+    } else {
+        value = _decoder(value)
+    }
+
+    return value
+}
+
+exports.schema = {
+    type: "string-base32",
+}
---
+++ javascript/test/coders/intstr-base32.js
@@ -0,0 +1,159 @@
+/*
+ *  test/coders/intstr-base32.js
+ *
+ *  Vitor Pamplona
+ *  PathCheck Foundation
+ *  2021-03-30
+ *
+ *  Copyright (2013-2021) Consensas
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("lodash")
+const jsonxt = require("../..")
+
+const fs = require("fs")
+const path = require("path")
+const assert = require("assert")
+
+const _util = require("./../_util")
+
+const WRITE = process.env.WRITE === "1"
+const DUMP = process.env.DUMP === "1"
+
+const TESTCLASS = "intstr-base32";
+
+describe("coders/intstr-base32", function() {
+    before(function() {
+        _util.shims_on()
+    })
+
+    after(function() {
+        _util.shims_off()
+    })
+
+    const rule = { 
+    }
+
+    it("works - zero", function() {
+        const start = '0'
+        const got_encoded = jsonxt.encoders[TESTCLASS](rule, start)
+        const got_decoded = jsonxt.decoders[TESTCLASS](rule, got_encoded)
+        const want = '0'
+
+        if (DUMP) {
+            console.log("")
+            console.log("start:", start)
+            console.log("encoded:", got_encoded)
+        }
+
+        assert.strictEqual(got_encoded, want)
+        assert.strictEqual(got_decoded, start)
+    })
+
+    it("works - positive number", function() {
+        const start = "2"
+        const got_encoded = jsonxt.encoders[TESTCLASS](rule, start)
+        const got_decoded = jsonxt.decoders[TESTCLASS](rule, got_encoded)
+        const want = "2"
+
+        if (DUMP) {
+            console.log("")
+            console.log("start:", start)
+            console.log("encoded:", got_encoded)
+        }
+
+        assert.strictEqual(got_encoded, want)
+        assert.strictEqual(got_decoded, start)
+    })
+
+    it("works - negative number", function() {
+        const start = "-2"
+        const got_encoded = jsonxt.encoders[TESTCLASS](rule, start)
+        const got_decoded = jsonxt.decoders[TESTCLASS](rule, got_encoded)
+        const want = "-2"
+
+        if (DUMP) {
+            console.log("")
+            console.log("start:", start)
+            console.log("encoded:", got_encoded)
+        }
+
+        assert.strictEqual(got_encoded, want)
+        assert.strictEqual(got_decoded, start)
+    })
+
+    it("works - null", function() {
+        const start = null
+        const got_encoded = jsonxt.encoders[TESTCLASS](rule, start)
+        const got_decoded = jsonxt.decoders[TESTCLASS](rule, got_encoded)
+        const want = "$."
+
+        if (DUMP) {
+            console.log("")
+            console.log("start:", start)
+            console.log("encoded:", got_encoded)
+        }
+
+        assert.strictEqual(got_encoded, want)
+        assert.strictEqual(got_decoded, start)
+    })
+
+    it("works - undefined", function() {
+        const start = undefined
+        const got_encoded = jsonxt.encoders[TESTCLASS](rule, start)
+        const got_decoded = jsonxt.decoders[TESTCLASS](rule, got_encoded)
+        const want = ""
+
+        if (DUMP) {
+            console.log("")
+            console.log("start:", start)
+            console.log("encoded:", got_encoded)
+        }
+
+        assert.strictEqual(got_encoded, want)
+        assert.strictEqual(got_decoded, start)
+    })
+
+    it("expected fail - string", function() {
+        const start = "1"
+
+        assert.rejects(async () => {
+            const got_encoded = jsonxt.encoders[TESTCLASS](rule, start)
+        })
+    })
+    it("expected fail - float", function() {
+        const start = 3.14
+
+        assert.rejects(async () => {
+            const got_encoded = jsonxt.encoders[TESTCLASS](rule, start)
+        })
+    })
+    it("expected fail - float string", function() {
+        const start = "3.14"
+
+        assert.rejects(async () => {
+            const got_encoded = jsonxt.encoders[TESTCLASS](rule, start)
+        })
+    })
+    it("expected fail - boolean", function() {
+        const start = true
+
+        assert.rejects(async () => {
+            const got_encoded = jsonxt.encoders[TESTCLASS](rule, start)
+        })
+    })
+})
